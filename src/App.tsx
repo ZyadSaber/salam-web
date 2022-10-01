2833e08e02c140ff27dc8f83d99e4a74c3f5c713
Zyad <Zyad>
2022-10-02 00:08:04 +0200

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { memo } from 'react';
-import Welcome from './Pages/WelcomePage/component'
+import Welcome from './Pages/WelcomePage/component';
+import Footer from './components/Footer/component';
+import Header from './components/Header/component';
+import Customers from './Pages/Customers/component';
+//@ts-ignore
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <Welcome />
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/customers" component={Customers} />
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
 
 export default memo(App);
+
---
+++ src/Pages/Customers/component.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const Customers = () => {
+    return (
+        <>
+            <h1>Welcome Customers</h1>
+        </>
+    )
+};
+
+export default memo(Customers);
