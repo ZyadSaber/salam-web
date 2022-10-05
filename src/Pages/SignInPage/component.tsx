210ed5b76a51c0560e32ebe9ec048ad87e3d709b
Zyad <Zyad>
2022-10-05 02:17:08 +0200

    designing log in page

--- src/Pages/SignInPage/component.tsx
+++ src/Pages/SignInPage/component.tsx
@@ -1,11 +1,19 @@
-import React, { memo } from 'react';
+import  { memo } from 'react';
 import './Style.css'
 
 const SignInPage = () => {
     return (
-        <>
-            <h1>Welcome Page</h1>
-        </>
+        <div className="card">
+            <div className="card-body">
+                <h1>Welcome ...</h1>
+                <input type="text" placeholder="User Name" />
+                <input type="password" placeholder="Password" />
+                <div className="btns">
+                    <button>Log In</button>
+                    <button>Exit</button>
+                </div>
+            </div>
+        </div>
     )
 };
 
