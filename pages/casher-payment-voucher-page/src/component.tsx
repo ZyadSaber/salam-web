bf415ff1138e9eebb5e7a7a91ac8b2ce019dd7b2
Zyad Saber <zyad.2002.best@gmail.com>
2023-04-28 11:17:02 +0300

    finish casher-payment-voucher-page

---
+++ pages/casher-payment-voucher-page/src/component.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+import Flex from "@commons/flex";
+import InputText from "@commons/input-text/src/inputText";
+import { ApiTable } from "@commons/table";
+import { columns } from "./constant";
+import { useFormManager } from "@commons/hooks";
+import ModalView from "./Partials/modalView"
+
+const CasherPaymentVoucher = () => {
+    const { state, onChange } = useFormManager({ initialValue: { date_from: "", date_to: "" } })
+    return (
+        <>
+            <Flex width="100%" flexDirection="column">
+                <Flex width="100%" >
+                    <InputText name="date_from" type="date" Label="from" onChange={onChange} />
+                    <InputText name="date_to" type="date" Label="to" onChange={onChange} />
+                </Flex>
+                <ApiTable
+                    api={"QUERY_CASHER_PAYMENT_VOUCHER_TABLE_DATA"}
+                    postApi={"POST_CASHER_PAYMENT_VOUCHER_TABLE_DATA"}
+                    columns={columns}
+                    hideTools={false}
+                    canEdit={true}
+                    canAdd={true}
+                    canDelete={true}
+                    rowKey={"payment_voucher_id"}
+                    params={{
+                        date_from: state.date_from,
+                        date_to: state.date_to
+                    }}
+                    Modal={ModalView}
+                    fetchOnFirstRun
+                />
+            </Flex>
+        </>
+    )
+}
+
+export default memo(CasherPaymentVoucher)
---
+++ pages/casher-payment-voucher-page/src/constant.ts
@@ -0,0 +1,43 @@
+export const columns = [
+        {
+            title: "dt",
+            dataIndex: "date",
+            width: "10%"
+        },
+        {
+            title: "amnt",
+            dataIndex: "amount",
+            width: "10%"
+        },
+        {
+            title: "vchrnm",
+            dataIndex: "voucher_name",
+            width: "10%"
+        },
+        {
+            title: "cstmrnm",
+            dataIndex: "customer_name",
+            width: "15%"
+        },
+        {
+            title: "splrnm",
+            dataIndex: "supplier_name",
+            width: "15%"
+        },
+        {
+            title: "nts",
+            dataIndex: "notes",
+            width: "10%"
+        }
+    ]
+
+export const voucherOptions = [
+    {
+        label: "splr",
+        value: 1,
+    },
+    {
+        label: "cstmr",
+        value: 2,
+    }
+]
